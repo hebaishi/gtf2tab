338805a62854d3f23cb39d9fc6ec44b19da31c02
hebaishi <hebaishi.husam@gmail.com>
2016-02-14 20:26:20 +0000

    Improve travis deploy, move build command to .travis.yml, slight CMakeLists.txt cleanup

---
+++ create_archive.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+TRAVIS_OS_NAME="linux"
+
+BINS="gtf2tab"
+
+LINUX_COMMAND="tar cvzf"
+LINUX_OUTPUT="gtf2tab_x86_64.tar.gz"
+
+cd bin
+if [ "$TRAVIS_OS_NAME" = "linux" ]
+then
+  $LINUX_COMMAND $LINUX_OUTPUT $BINS
+fi
